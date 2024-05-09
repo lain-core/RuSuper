bc1eb5830173f09a7baa458c97fd42d92a6212b8
Hunter King <HDK@cambridge-design.co.uk>
2024-05-09 16:35:03 -0400

    Test checksum completed

--- src/romdata.rs
+++ src/romdata.rs
@@ -27,8 +27,8 @@ const HDR_MAP_MODE_LEN:                 usize = 1;
 const HDR_CART_TYPE_LEN:                usize = 1;
 const HDR_ROM_SIZE_LEN:                 usize = 1;
 const HDR_RAM_SIZE_LEN:                 usize = 1;
-const HDR_DEST_CODE_LEN:                usize = 1;
-const HDR_FIXED_VAL_LEN:                usize = 1;
+const HDR_DEST_CODE_LEN:                usize = 1;  // Used to determine the region.
+const HDR_FIXED_VAL_LEN:                usize = 1;  // Used to determine if the optional header is present.
 const HDR_MASK_ROM_VER_LEN:             usize = 1;
 const HDR_COMPLEMENT_CHECK_LEN:         usize = 2;
 const HDR_CHECKSUM_LEN:                 usize = 2;
@@ -44,7 +44,10 @@ const HDR_FIXED_VAL_INDEX:              usize = HDR_DEST_CODE_INDEX + HDR_DEST_C
 const HDR_MASK_ROM_VER_INDEX:           usize = HDR_FIXED_VAL_INDEX + HDR_FIXED_VAL_LEN;
 const HDR_COMPLEMENT_CHECK_INDEX:       usize = HDR_MASK_ROM_VER_INDEX + HDR_MASK_ROM_VER_LEN;
 const HDR_CHECKSUM_INDEX:               usize = HDR_COMPLEMENT_CHECK_INDEX + HDR_COMPLEMENT_CHECK_LEN;
+
 const HDR_TEST_VALUE:                   u16 = 0xFFFF; // A test checksum + the complement value should equal this value.
+const HDR_OPT_PRESENT:                  u8  = 0x33;   // Value in HDR_FIXED_VAL_INDEX if the optional header is present.
+const HDR_SUBTYPE_PRESENT:              u8  = 0x00;   // Value in HDR_FIXED_VAL_INDEX if only the chipset subtype is present. 
 
 /// Exception Vector Breakdown (16 bytes)
 const _EV_NATIVE_UNUSED_1_LEN:          usize = 4;
@@ -104,8 +107,8 @@ pub const ROM_BASE_ADDR:                u16 = 0x8000;    // All LoRom banks, and
 
 /********************************* ROM Info Enums & Struct Definitions *********************************/
 /// Info pertaining to the memory map and size of the ROM.
+#[derive(Debug, Clone, Copy)]
 pub enum RomSize {
-    ExLoRom,
     LoRom,
     HiRom,
     ExHiRom
@@ -114,6 +117,7 @@ pub enum RomSize {
 /// Info pertaining to the CPU clock speed the SNES runs at for this ROM.
 /// SlowRom = 2.68MHz
 /// FastRom = 3.58MHz
+#[derive(Debug, Clone, Copy)]
 pub enum RomClkSpeed {
     SlowRom,    // 2.68MHz
     FastRom     // 3.58MHz
@@ -121,6 +125,7 @@ pub enum RomClkSpeed {
 
 /// ROM Expansion chips which are noted and affect memory map.
 /// Note that SuperFX and others have their own memory map which is not covered by this.
+#[derive(Debug, Clone, Copy)]
 pub enum RomExpansions {
     None,
     SA1,
@@ -216,6 +221,7 @@ pub struct RomData {
     opt_header: OptionalHeader,
     exception_vectors: ExceptionVectorTable,
     opt_is_present: bool,
+    mem_map: RomSize
 }
 
 impl RomData{
@@ -225,7 +231,8 @@ impl RomData{
             header: [0; HDR_LEN_BYTES],
             opt_header: [0; OPT_HEADER_LEN_BYTES],
             exception_vectors: [0; EV_LEN_BYTES],
-            opt_is_present: false
+            opt_is_present: false,
+            mem_map: RomSize::LoRom
         }
     }
 }
@@ -275,7 +282,9 @@ fn read_rom_to_buf(path: PathBuf) -> Result<Vec<u8>, RomReadError> {
     
     // Check and just immediatelyVirtualMachine flop if an SMC is passed until we manage that.
     if path.extension().unwrap() == "smc" {
-        return Err(RomReadError{ context: format!("SMC files contain additional data that is unneeded.\nThis tool does not support them yet. Please use an SFC file.")});
+        return Err(RomReadError::new(
+            format!("SMC files contain additional data that is unneeded.\nThis tool does not support them yet. Please use an SFC file.").to_string()
+        ));
     }
 
     // Attempt to open the file, and load it into a buffer.
@@ -286,11 +295,15 @@ fn read_rom_to_buf(path: PathBuf) -> Result<Vec<u8>, RomReadError> {
         let mut buf: Vec<u8> = vec![];
         let _read_result = match file.unwrap().read_to_end(&mut buf) {
             Ok(_) => { retval = Ok(buf); },
-            Err(e) => {retval = Err(RomReadError{ context: format!("Failed to read file: {}", e)});},
+            Err(e) => {
+                retval = Err(RomReadError::new(format!("Failed to read file: {}", e)));
+            },
         };
     }
     else{
-        retval = Err(RomReadError{ context: format!("Failed to open file at {}", &path.display()) });
+        retval = Err(RomReadError::new(
+            format!("Failed to open file at {}", &path.display())
+        ));
     }
 
     // If the file was read successfully, operate on it.
@@ -306,54 +319,87 @@ fn read_rom_to_buf(path: PathBuf) -> Result<Vec<u8>, RomReadError> {
 ///     - `HeaderData` struct with copies of the Header, Exception Vector, and Optional Header (if present) values.
 ///     - `RomReadError` if the 
 fn fetch_header(rom: &Vec<u8>) -> Result<RomData, RomReadError> {
-    let retval: Result<RomData, RomReadError>;
-    // Sum all bytes in the file. overflow is fine.
-    let checksum: Wrapping<u16> = Wrapping(0);
-    rom.iter().map(|x| checksum + Wrapping(*x as u16));
+    let mut retval: Result<RomData, RomReadError> = Err(RomReadError::new("".to_string()));
+    let header: Header = [0; HDR_LEN_BYTES];
 
-    let mut header: Header = [0; HDR_LEN_BYTES];
-    let mapping: Result<RomModeMapping, RomReadError>;
+    // Sum all bytes in the file. overflow is fine.
+    let mut checksum: Wrapping<u16> = Wrapping(0);
+    let _ = rom.iter().map(|x| checksum += Wrapping(*x as u16));
     
+    let mut checksum_valid: Result<RomSize, RomReadError>;
+
     // Check the bounds, then excise where a header would be and test for the mapping.
-    if rom.capacity() > HI_ROM_BANK_SIZE_BYTES
-    {
-        header.clone_from_slice(&rom[HI_ROM_HEADER_ADDR .. (HI_ROM_HEADER_ADDR + HDR_LEN_BYTES)]);
-        match test_checksum(checksum.0, &header){
-            Ok(_t) => { mapping = gather_mapping(&header); },
-            Err(e) => { retval = Err(e); }
+    // If the ROM is > HI_ROM_BANK_SIZE_BYTES in length, it could be a hirom or a lorom.
+    if rom.capacity() >= HI_ROM_BANK_SIZE_BYTES {
+        // Test if this is a LoRom.
+        header.clone_from_slice(&rom[LO_ROM_HEADER_ADDR .. (LO_ROM_HEADER_ADDR + HDR_LEN_BYTES)]);
+        checksum_valid = test_checksum(checksum.0, &header);
+
+        if checksum_valid.is_err() {
+            // Test if this is a HiRom.
+            header.clone_from_slice(&rom[HI_ROM_HEADER_ADDR .. (HI_ROM_HEADER_ADDR + HDR_LEN_BYTES)]);
+            checksum_valid = test_checksum(checksum.0, &header);
         };
     }
-    else if rom.capacity() > LO_ROM_BANK_SIZE_BYTES
+    // Otherwise it could only possibly be a LoRom.
+    else if rom.capacity() >= LO_ROM_BANK_SIZE_BYTES
     {
+        // Test if this is a LoRom.
         header.clone_from_slice(&rom[LO_ROM_HEADER_ADDR .. (LO_ROM_HEADER_ADDR + HDR_LEN_BYTES)]);
-        match test_checksum(checksum.0, &header){
-            Ok(_t) => { mapping = gather_mapping(&header); },
-            Err(e) => { retval = Err(e); }
-        };
+        checksum_valid = test_checksum(checksum.0, &header);
     }
     else {
-        retval = Err(RomReadError{ context: "File was too small to contain header".to_string() });
+        checksum_valid = Err(RomReadError{ context: "File was too small to contain header".to_string() });
     }
 
-    Err(RomReadError{context: "Function implementation is not complete".to_string() })
+    // If we got a valid checksum, use the returned size to populate a new RomData object.
+    if checksum_valid.is_ok() {
+        let opt_header_is_present = &header[HDR_FIXED_VAL_INDEX] == ;
+
+        retval = Ok(RomData {
+            header: header,
+            opt_header: 
+        });
+    }
+
+    return retval;
 }
+
 
 fn gather_mapping(header: &Header) -> Result<RomModeMapping, RomReadError> {
     Ok(RomModeMapping::new())
 }
 
-fn test_checksum(checksum: u16, header: &Header) -> Result<(), RomReadError> {
+/// Test if the checksum for this file is valid, and if so, check the map byte and return the result.
+/// # Parameters:
+///     - `checksum`:   u16 sum of all bytes in the file, with overflow discarded.
+///     - `header`:     The header to analyze.
+/// # Returns:
+///     - `RomSize`:        If the ROM checksum was valid,
+///     - `RomReadError`:   If the ROM checksum was invalid, with both the calculated and internal values printed.
+fn test_checksum(checksum: u16, header: &Header) -> Result<RomSize, RomReadError> {
     let test_checksum: [u8; 2] = header[HDR_CHECKSUM_INDEX .. HDR_CHECKSUM_INDEX + 1].try_into().unwrap();
     let test_compare: [u8; 2] = header[HDR_COMPLEMENT_CHECK_INDEX .. HDR_COMPLEMENT_CHECK_INDEX + 1].try_into().unwrap();
-
-
+    
+    let mut retval: Result<RomSize, RomReadError> = Err(RomReadError::new(
+        format!("ROM Checksum was invalid.\nCalculated Checksum: {:#06X}\nROM Checksum: {:#06X}", checksum, test_checksum.to_word()).to_string()
+    ));
     if checksum == test_checksum.to_word() && 
         ((Wrapping(checksum) + Wrapping(test_compare.to_word())).0 == HDR_TEST_VALUE) {
-        // This rom looks good. Actually fetch the values for this.
-        println!("Checksum was good!");
+        // This rom looks good. Get the size of the rom and throw it back out.
+        let rom_map_mode: u8 = header[HDR_MAP_MODE_INDEX];
+        if (rom_map_mode & MAP_EXHIROM_MASK) != 0 {
+            retval = Ok(RomSize::ExHiRom);
+        }
+        else if (rom_map_mode & MAP_HIROM_MASK) != 0 {
+            retval = Ok(RomSize::HiRom);
+        }
+        else if (rom_map_mode & MAP_BASE_MASK) != 0 {
+            retval = Ok(RomSize::LoRom);
+        }
     }
 
-    Ok(())
+    return retval;
 }
 
 /********************************* ROM Info Tests ******************************************************/
