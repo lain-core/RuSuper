bc386caf8f94bb506b73b7c64c21f57c2ce17170
lain-core <HDK@cambridge-design.co.uk>
2024-07-23 16:20:21 -0400

    Overcomplicating carry bit. Need to implement v flag set

--- src/cpu/instructions/arithmetic.rs
+++ src/cpu/instructions/arithmetic.rs
@@ -7,7 +7,7 @@ use std::num::Wrapping;
 
 use super::{
     memory,
-    registers::{self, ALU_8BIT_CARRY_BIT},
+    registers::{self, ALU_16BIT_CARRY_BIT, ALU_8BIT_CARRY_BIT},
     CpuState,
 };
 
@@ -15,8 +15,10 @@ use super::{
 /// Syntax: ADC #const
 /// Opcode: 0x6D
 /// Bytes:  2 if 8-bit param, 3 if 16-bit
-/// Extra: +1 if
+/// Flags affected: nv----zc
 pub(super) fn adc_immediate(cpu: &mut CpuState, _memory: &mut memory::Memory, param: u16) -> bool {
+    // If the carry flag is already set
+
     // If the operation is in 8-bit mode, then perform all of the math in a u8 context.
     if cpu.registers.status.flags[registers::STATUS_AREG_SIZE_BIT] as usize
         == registers::REGISTER_MODE_8_BIT
@@ -24,27 +26,33 @@ pub(super) fn adc_immediate(cpu: &mut CpuState, _memory: &mut memory::Memory, pa
         let acc_value: u8 = (cpu.registers.acc.0 & 0x00FF) as u8;
         let param_value: u8 = (param & 0x00FF) as u8;
         match acc_value.checked_add(param_value) {
-            Some(value) => {}
+            Some(value) => {
+                cpu.registers.acc = Wrapping(value as u16);
+                cpu.registers.status.flags[registers::STATUS_CARRY_BIT] = false;
+            }
             None => {
-                cpu.registers.status.flags[registers::STATUS_CARRY_BIT] =
-                    (((acc_value + param_value) as u16 & (1 << ALU_8BIT_CARRY_BIT))
-                        >> ALU_8BIT_CARRY_BIT)
-                        != 0;
+                //     (((acc_value + param_value) as u16 & (1 << ALU_8BIT_CARRY_BIT))
+                //         >> ALU_8BIT_CARRY_BIT)
+                //         != 0;
                 cpu.registers.acc = Wrapping(acc_value.wrapping_add(param_value) as u16);
+                cpu.registers.status.flags[registers::STATUS_CARRY_BIT] = true;
             }
         }
     }
     // If the operation is in 16-bit mode, perform the math that way.
-
-    // Check if the addition will overflow. If not, proceed as usual.
-    match cpu.registers.acc.0.checked_add(param) {
-        Some(_value) => {
-            cpu.registers.acc += param;
-        }
-        // Otherwise
-        None => {
-            cpu.registers.acc += param;
-            cpu.registers.status.flags[registers::STATUS_CARRY_BIT] = true;
+    else {
+        match cpu.registers.acc.0.checked_add(param) {
+            Some(value) => {
+                cpu.registers.acc = Wrapping(value);
+                cpu.registers.status.flags[registers::STATUS_CARRY_BIT] = false;
+            }
+            None => {
+                // ((cpu.registers.acc + Wrapping(param)).0 as u32
+                //     & (1 << ALU_16BIT_CARRY_BIT) >> ALU_16BIT_CARRY_BIT)
+                //     != 0;
+                cpu.registers.acc += Wrapping(param);
+                cpu.registers.status.flags[registers::STATUS_CARRY_BIT] = true;
+            }
         }
     }
 
